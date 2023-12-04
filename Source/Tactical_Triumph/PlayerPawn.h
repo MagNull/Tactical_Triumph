b85f0ce98b44ece4d9b479e32c95c3e8e58ecde0
iv_tupikov <i.tupikov12@gmail.com>
2023-12-04 19:45:12 +0500

    Drag&Drop mechanic

---
+++ Source/Tactical_Triumph/PlayerPawn.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "DA_Card.h"
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "PlayerPawn.generated.h"
+
+UCLASS()
+class TACTICAL_TRIUMPH_API APlayerPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	APlayerPawn();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+	
+protected:
+	
+	UPROPERTY()
+	class UHandle* handle_;
+
+public:	 
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UFUNCTION(BlueprintCallable)
+	TArray<UDA_Card*> GetCards() const;
+};
--- Source/Tactical_TriumphEditor.Target.cs
+++ Source/Tactical_TriumphEditor.Target.cs
@@ -1,11 +1,10 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
-using System.Collections.Generic;
 
 public class Tactical_TriumphEditorTarget : TargetRules
 {
-	public Tactical_TriumphEditorTarget( TargetInfo Target) : base(Target)
+	public Tactical_TriumphEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V4;
